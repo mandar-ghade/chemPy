f13a81463eb3a4218b2ac742c45fe24a70aa54e3
mandarghade <gx2h912@gmail.com>
2023-12-16 22:02:58 -0500

    Deleted `strip_coefs.py` file.

--- chempy/utils/strip_coefs.py
+++
@@ -1,6 +0,0 @@
-def strip_coefficients(comp_str: str) -> str:
-    comp_str = comp_str.strip()
-    for i, c in enumerate(comp_str):
-        if c.isdigit():
-            continue
-        return comp_str[i:]
